96fdfd54f3ea51bc8e3ec38ed6841bdf4a3d622a
Rp70 <14834373+Rp70@users.noreply.github.com>
2019-06-01 18:52:07 +0700

    Updates

--- versions/7.1/files/docker-entrypoint-init.d/00_entrypoint.sh
+++ versions/7.1/files/docker-entrypoint-init.d/00_entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
-#if [ "$PHP_VERSION" = '5.4' ]; then
+if [ "$PHP_MINOR_VERSION" = '5.4' ]; then
 	LISTEN_ADDRESS=${LISTEN_ADDRESS:='9000'}
-#fi
+	touch /usr/local/etc/php-fpm.d/zz-docker.conf
+fi

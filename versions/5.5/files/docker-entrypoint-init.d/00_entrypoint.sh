6ec979c6a9651f4623b9e9c42be935f951109354
Rp70 <14834373+Rp70@users.noreply.github.com>
2019-07-20 16:29:13 +0700

    PHP 5.4, fix: ALERT: [pool www] no listen address have been defined!

--- versions/5.5/files/docker-entrypoint-init.d/00_entrypoint.sh
+++ versions/5.5/files/docker-entrypoint-init.d/00_entrypoint.sh
@@ -3,5 +3,6 @@ set -e
 
 if [ "$PHP_MINOR_VERSION" = '5.4' ]; then
 	LISTEN_ADDRESS=${LISTEN_ADDRESS:='9000'}
+	sed -i -e "s|^;listen =.*|listen = $LISTEN_ADDRESS|" /usr/local/etc/php-fpm.conf
 	#touch /usr/local/etc/php-fpm.d/zz-docker.conf
 fi

cdf0ce8e27a851cce133ba0040c3ac6990fb125d
Rp70 <lequocdung99vn@yahoo.com>
2023-06-11 03:01:01 +0000

    refactor: allow enable XDEBUG with values of 'yes', 'on', 'true'

--- versions/7.0/init.d/xdebug.sh
+++ versions/7.0/init.d/xdebug.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Enable/disable xdebug
-if [ "$USE_XDEBUG" = "yes" ]; then
+if [ "$USE_XDEBUG" = "yes" -o "$USE_XDEBUG" = "true" -o "$USE_XDEBUG" = "on" ]; then
     cp -n /usr/local/etc/php/xdebug.d/* /usr/local/etc/php/conf.d/
 else
     find /usr/local/etc/php/conf.d -name '*xdebug*' -delete
